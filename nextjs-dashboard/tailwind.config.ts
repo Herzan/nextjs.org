4aa252b1e5111904d3acc4d8363f4794948dd330
Herzan <46695573+Herzan@users.noreply.github.com>
2026-05-19 23:46:42 -0600

    Fix missing dependencies

--- nextjs-dashboard/tailwind.config.ts
+++ nextjs-dashboard/tailwind.config.ts
@@ -7,26 +7,9 @@ const config: Config = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      gridTemplateColumns: {
-        '13': 'repeat(13, minmax(0, 1fr))',
-      },
-      colors: {
-        blue: {
-          400: '#2589FE',
-          500: '#0070F3',
-          600: '#2F6FEB',
-        },
-      },
-    },
-    keyframes: {
-      shimmer: {
-        '100%': {
-          transform: 'translateX(100%)',
-        },
-      },
-    },
+    extend: {},
   },
   plugins: [require('@tailwindcss/forms')],
 };
+
 export default config;
