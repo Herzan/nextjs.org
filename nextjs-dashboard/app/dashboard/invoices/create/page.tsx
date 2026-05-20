a14c364d24c875cfda454e8c86e22b3754bbc87f
Herzan <46695573+Herzan@users.noreply.github.com>
2026-05-19 20:33:29 -0600

    11

---
+++ nextjs-dashboard/app/dashboard/invoices/create/page.tsx
@@ -0,0 +1,42 @@
+import Form from '@/app/ui/invoices/create-form';
+import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
+import { fetchCustomers } from '@/app/lib/data';
+import { createInvoice } from '@/app/lib/actions';
+import { CustomerField } from '@/app/lib/definitions';
+
+export default async function Page() {
+  const customers: CustomerField[] = await fetchCustomers();
+
+  return (
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: 'Invoices', href: '/dashboard/invoices' },
+          {
+            label: 'Create Invoice',
+            href: '/dashboard/invoices/create',
+            active: true,
+          },
+        ]}
+      />
+
+      <form action={createInvoice}>
+        <div>
+          <label htmlFor="customer">Customer</label>
+
+          <select id="customer" name="customerId">
+            <option value="">Select a customer</option>
+
+            {customers.map((customer) => (
+              <option key={customer.id} value={customer.id}>
+                {customer.name}
+              </option>
+            ))}
+          </select>
+        </div>
+
+        <button type="submit">Create Invoice</button>
+      </form>
+    </main>
+  );
+}
